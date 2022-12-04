96e763ea1215a6a1ddab4b387be5c4d710925e85
Alfred Meshcheryakov <alfred.m@itbegin.ru>
2022-12-04 19:19:58 +0300

    changed build steps

---
+++ RADME.md
@@ -0,0 +1,1 @@
+This repository is for the Build a Java app with Maven in Teamcity + Nexus (Build: 1.Single APP; 2. Docker).
--- teamcity_java_projects/Docker_java_build.kts
+++ teamcity_java_projects/Docker_java_build.kts
@@ -6,41 +6,13 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.DockerCommandStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
-object JavaBuild : BuildType({
+object DockerJavaBuild : BuildType({
     name = "Docker_java_build"
 
-    params {
-        password("DockerPass", "******")
-        param("DockerUser", "admin")
-    }
-
     vcs {
-        root(DokcerSimpleJavaMavenApp)
+        root(HttpsGithubComFred13SimpleJavaMavenAppGitRefsHeadsMain)
     }
-
     steps {
-        script {
-            name = "Create_Dockerfile"
-            scriptContent = """
-                cat <<EOF > Dockerfile
-                #
-                # Build stage
-                #
-                FROM maven:3.6.3-jdk-11-slim AS build
-                WORKDIR /usr/src/app
-                COPY . ./
-                RUN mvn clean package
-
-                #
-                # Package stage
-                #
-                FROM openjdk:11-jre-slim
-                WORKDIR /usr/src/app
-                COPY --from=build /usr/src/app/target/my-app-1.0-SNAPSHOT.jar ./app.jar
-                CMD ["java","-jar", "./app.jar"]
-                EOF
-            """.trimIndent()
-        }
         dockerCommand {
             name = "Build_docker_image"
             commandType = build {
@@ -64,10 +36,8 @@ object JavaBuild : BuildType({
             }
         }
     }
-
     features {
         dockerSupport {
-            cleanupPushedImages = true
             loginToRegistry = on {
                 dockerRegistryId = "PROJECT_EXT_2"
             }
